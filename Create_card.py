a7553d96c6338f712c2b4ff1584655e265b48c8f
Krystalgit <674398228@qq.com>
2023-05-04 15:07:20 +0800

    h16

--- Create_card.py
+++ Create_card.py
@@ -6,7 +6,7 @@ import streamlit as st
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
 import os
-os.system("pip install " + 'BeautifulSoup4')
+# os.system("pip install " + 'BeautifulSoup4')
 import re
 from bs4 import BeautifulSoup as bs
 from io import BytesIO
